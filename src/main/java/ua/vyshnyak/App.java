218fbdded8c110bf0f5b0c2a99fad76d0d9c5efd
Maksym Vyshnyak <dbiyzr13@gmail.com>
2019-11-30 14:34:08 +0200

    MT-0000 initial commit

---
+++ src/main/java/ua/vyshnyak/App.java
@@ -0,0 +1,13 @@
+package ua.vyshnyak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
